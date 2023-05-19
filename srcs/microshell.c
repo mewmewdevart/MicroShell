7b0a7d22e79946de81eb3c0b9a971a919dccec1b
Larcrist <larcrist@student.42sp.org.nr>
2023-05-19 17:49:44 -0300

    feat (echo): Testing the echo function and readline to start

--- srcs/microshell.c
+++ srcs/microshell.c
@@ -1,7 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larcrist <larcrist@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 17:20:51 by larcrist          #+#    #+#             */
+/*   Updated: 2023/05/19 17:26:59 by larcrist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/microshell.h"
 
-int  main(void)
+int	main(void)
 {
+	char	*input;
 
-    return (0);
+	input = "";
+	while (ft_strncmp(input, "exit", 5) != 0)
+	{
+		input = readline("$ ");
+		if (ft_strncmp(input, "echo", 4))
+			ft_echo(&input[5]);
+	}
+	return (0);
 }
