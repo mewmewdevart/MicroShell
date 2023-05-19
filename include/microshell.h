71556125e85fc50695e9d05c55dcbd5f36b91346
Larcrist <larcrist@student.42sp.org.nr>
2023-05-19 17:48:59 -0300

    feat (include) : add the function echo in the header

--- include/microshell.h
+++ include/microshell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larcrist <larcrist@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 17:21:56 by larcrist          #+#    #+#             */
+/*   Updated: 2023/05/19 17:45:06 by larcrist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
@@ -5,8 +17,10 @@
 
 # include <stdio.h>      // printf
 # include <stdlib.h>     // malloc, free, exit
-# include <unistd.h>     // write, access, open, read, close, fork, execve, dup, dup2, pipe
-# include <sys/types.h>  // wait, waitpid, wait3, wait4, stat, lstat, fstat, unlink
+# include <unistd.h>     // write, access, open, read, close,
+// fork, execve, dup, dup2, pipe
+# include <sys/types.h>  // wait, waitpid, wait3, wait4,
+// stat, lstat, fstat, unlink
 # include <sys/wait.h>   // wait, waitpid, wait3, wait4
 # include <signal.h>     // signal, sigaction, sigemptyset, sigaddset, kill
 # include <errno.h>      // strerror
@@ -18,6 +32,12 @@
 # include <curses.h>     // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
 # include <pwd.h>        // getpwuid
 
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+
 void	ft_errors_init(int number_error);
+
+void	ft_echo(char *input);
 
 #endif
